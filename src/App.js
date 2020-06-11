94ae28cdb31b7c52898995a654612c1b5931ada8
@fedoskamcha <korolevfedos@yandex.ru>
2020-06-11 12:15:04 +1200

    Example

--- src/App.js
+++ src/App.js
@@ -3,20 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const message = encodeURIComponent('test')
+  //const phone = '7XXXXXXXXXX'
+  const phone = '79619671404'  
+  const url = `https://wa.me/${phone}?text=${message}`
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Покрешить whatsapp ui:</h1>
+        <ul className='App-list'>
+          <li>в App.js -> phone = задать номер телефона</li>
+          <li>1) открыть приложение на ios</li>
+          <li>2) открыть чат с номером телефона (phone)</li>
+          <li>3) закрыть приложение полностью</li>
+          <li>4) в safari  перейти по ссылке 'Ломай'.</li>
+          <li>5) Koнец.</li>
+        </ul>
         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          href={url}
+          target="a"
         >
-          Learn React
+          Ломай
         </a>
       </header>
     </div>
